d81a310915133786e4df8ad9018a5dd08e68a49d
luxjoshyua <josh.e.fielding@gmail.com>
2020-02-29 15:58:59 +1100

    changed background colour

--- src/js/stamps/index.js
+++ src/js/stamps/index.js
@@ -1,15 +1,17 @@
 let number = 0
 
 const stamps = [
-    "./assets/shapes/circles.svg",
-    "./assets/shapes/heart.svg",
-    "./assets/shapes/moon.svg",
-    "./assets/shapes/rainbow.svg",
-    "./assets/shapes/shooting-star.svg",
-    "./assets/shapes/waves.svg"
+    // "./assets/shapes/circles.svg",
+    "../assets/shapes/circles.svg",
+    "../assets/shapes/heart.svg",
+    "../assets/shapes/moon.svg",
+    "../assets/shapes/rainbow.svg",
+    "../assets/shapes/shooting-star.svg",
+    "../assets/shapes/waves.svg"
 ]
 
 const stampsTag = document.querySelector(".stamps")
+console.log(stampsTag);
 
 const addStamp = function (x, y) {
 
@@ -17,8 +19,6 @@ const addStamp = function (x, y) {
     const img = document.createElement("img")
 
     img.setAttribute("src", stamps[number])
-    // moved into the style CSS
-    // img.style.position = "absolute"
     // remove half the window width so it's centered 
     img.style.left = (x - window.innerWidth / 2) + "px"
     img.style.top = y + "px"
@@ -26,7 +26,7 @@ const addStamp = function (x, y) {
 
     // add some audio
     const audio = document.createElement("audio")
-    audio.setAttribute("src", "assets/plop.mp3")
+    audio.setAttribute("src", "assets/shapes/plop.mp3")
     audio.play()
 
     number = number + 1
@@ -35,7 +35,6 @@ const addStamp = function (x, y) {
         number = 0
     }
 }
-
 
 document.addEventListener("click", function () {
     // console.log(event)
