091599e094226d1b884613743297c72a5087224d
luxjoshyua <josh.e.fielding@gmail.com>
2020-03-10 06:57:27 +1100

    added custom cursor and new hero image

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import SmoothScrollingArticle, {
 import "./js/elements-move";
 import "./js/gallery";
 import "./js/nav";
+// import "./js/cursor";
 
 
 
@@ -32,14 +33,14 @@ parallaxScroll.forEach(element => {
   });
   const change = element.getAttribute("data-max-change") ?
     element.getAttribute("data-max-change") :
-    50;
+    80;
   // console.log(change);
 
   item.add({
     ease: 0.1,
     render: (title, currentPosition) => {
       //currentPosition is number from 0 to 1
-      const val = map(currentPosition, 0, 0.6, change, change * -1);
+      const val = map(currentPosition, 0, 0.9, change, change * -1);
       title.style.transform = `translate3d(0,${val}px,0)`;
       title.style.opacity = currentPosition;
     }
@@ -49,25 +50,25 @@ parallaxScroll.forEach(element => {
 
 
 // zoom scroll
-const zoomScroll = document.querySelectorAll(".zoom-scroll");
+// const zoomScroll = document.querySelectorAll(".zoom-scroll");
 
-zoomScroll.forEach(element => {
-  const zoomItem = new Item({
-    element
-  });
-  zoomItem.add({
-    ease: 0.1, //ease is how smooth you want the animation to move lower is smoother, but may cause performance issues.
-    render: (image, current) => {
-      //current will be 0-1
-      //render receives 2 params, the element, and the current position on page from bottom to top of screen
-      //bottom is 0
-      //top is 1
-      const scale = map(current, 0, 1, 1, 1.1);
-      image.style.transform = `scale3d(${scale},${scale},1)`;
-    }
-  });
-  items.push(zoomItem);
-});
+// zoomScroll.forEach(element => {
+//   const zoomItem = new Item({
+//     element
+//   });
+//   zoomItem.add({
+//     ease: 0.1, //ease is how smooth you want the animation to move lower is smoother, but may cause performance issues.
+//     render: (image, current) => {
+//       //current will be 0-1
+//       //render receives 2 params, the element, and the current position on page from bottom to top of screen
+//       //bottom is 0
+//       //top is 1
+//       const scale = map(current, 0, 1, 1, 1.1);
+//       image.style.transform = `scale3d(${scale},${scale},1)`;
+//     }
+//   });
+//   items.push(zoomItem);
+// });
 
 new SmoothScrollingArticle({
   article: document.querySelector("article"),
