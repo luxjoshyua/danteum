56fb5fbceff59889774d710d2894244a915c4e7a
Joshua Fielding <Joshua.Fielding@fairfaxmedia.com.au>
2020-03-24 16:36:42 +1100

    canvas tag experimentation

--- src/js/collage/index.js
+++ src/js/collage/index.js
@@ -1,5 +1,14 @@
 const canvasTag = document.querySelector('.collage-draw');
 import img1 from "../../assets/collage/06.jpg";
+import img2 from "../../assets/collage/15.jpg";
+import img3 from "../../assets/collage/18.jpg";
+import img4 from "../../assets/collage/19.jpg";
+import img5 from "../../assets/collage/28.jpg";
+import img6 from "../../assets/collage/31.png";
+import img7 from "../../assets/collage/55.png";
+import img8 from "../../assets/collage/58.jpg";
+import img9 from "../../assets/collage/62.jpg";
+
 const collageWrapper = document.querySelector('.collage-wrapper');
 
 
@@ -15,15 +24,71 @@ canvasTag.style.height = window.innerHeight + "px";
 const context = canvasTag.getContext("2d")
 context.scale(2, 2)
 
-const image = document.createElement('img')
-image.src = img1;
+// const image = document.createElement('img')
+// image.src = img1;
 
+
+let aimX = null;
+let aimY = null;
+let currentX = null
+let currentY = null
+
+
+// i = index
+let i = 0;
+const images = [
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9
+    // .map takes the input and turns it into something new
+].map(src => {
+    const image = document.createElement('img')
+    image.src = src
+    return image
+})
 
 collageWrapper.addEventListener('mousemove', (event) => {
-    // complete checks if image has been loaded by browser
-
-    if (image.complete) {
-        context.drawImage(image, event.pageX - 200, event.pageY - 300, 400, 600)
+    aimX = event.pageX
+    aimY = event.pageY
+    if (currentX === null) {
+        currentX = event.pageX
+        currentY = event.pageY
     }
 
 })
+
+canvasTag.addEventListener('click', () => {
+    // 0 becomes 1 as soon as we click, click again 1 becomes 2 etc
+    i = i + 1
+    if (i >= images.length) {
+        i = 0;
+    }
+})
+
+
+const draw = () => {
+    if (currentX) {
+        // complete checks if image has been loaded by browser
+        // console.log("correctly logging", event)
+        if (images[i].complete) {
+            context.drawImage(images[i], currentX - 200, currentY - 300, 400, 600)
+        }
+
+        // 0.01 makes it super slow
+        currentX = currentX + (aimX - currentX) * 0.1
+        currentY = currentY + (aimY - currentY) * 0.1
+
+    }
+
+    // run draw on every frame
+    requestAnimationFrame(draw)
+}
+
+
+draw()
