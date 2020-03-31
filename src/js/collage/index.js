e05e0e7b122fa8614ab64ea01df446d80f50f74a
luxjoshyua <josh.e.fielding@gmail.com>
2020-04-01 09:10:05 +1100

    started interview DOM layout

--- src/js/collage/index.js
+++ src/js/collage/index.js
@@ -1,4 +1,4 @@
-const canvasTag = document.querySelector('.collage-draw');
+const canvasTag = document.querySelector(".collage-draw");
 import img1 from "../../assets/collage/06.jpg";
 import img2 from "../../assets/collage/15.jpg";
 import img3 from "../../assets/collage/18.jpg";
@@ -14,30 +14,27 @@ import img12 from "../../assets/collage/garfield.jpg";
 import img13 from "../../assets/collage/starfish.png";
 import img14 from "../../assets/collage/pussyriot.jpg";
 
-
-const collageWrapper = document.querySelector('.collage-wrapper');
-
+const collageWrapper = document.querySelector(".collage-wrapper");
 
 // window.innerWidth is how wide the screen is
-canvasTag.width = window.innerWidth * 2
-canvasTag.height = window.innerHeight * 2
+canvasTag.width = window.innerWidth * 2;
+
+canvasTag.height = window.innerHeight * 2;
 
 canvasTag.style.width = window.innerWidth + "px";
 canvasTag.style.height = window.innerHeight + "px";
 
 // context is the context you're drawing the canvas in e.g. 2d, 3d, 4d
-const context = canvasTag.getContext("2d")
-context.scale(2, 2)
+const context = canvasTag.getContext("2d");
+context.scale(2, 2);
 
 // const image = document.createElement('img')
 // image.src = img1;
 
-
 let aimX = null;
 let aimY = null;
-let currentX = null
-let currentY = null
-
+let currentX = null;
+let currentY = null;
 
 // i = index
 let i = 0;
@@ -58,32 +55,30 @@ const images = [
     img14
     // .map takes the input and turns it into something new
 ].map(src => {
-    const image = document.createElement('img')
-    image.src = src
-    return image
-})
+    const image = document.createElement("img");
+    image.src = src;
+    return image;
+});
 
-collageWrapper.addEventListener('mousemove', (event) => {
+collageWrapper.addEventListener("mousemove", event => {
     // aimX = event.pageX
-    aimX = event.offsetX
-    aimY = event.offsetY
+    aimX = event.offsetX;
+    aimY = event.offsetY;
     // aimY = event.pageY
     if (currentX === null) {
-        currentX = event.pageX
+        currentX = event.pageX;
         // console.log("Current X is", currentX)
-        currentY = event.pageY
+        currentY = event.pageY;
     }
+});
 
-})
-
-canvasTag.addEventListener('click', () => {
+canvasTag.addEventListener("click", () => {
     // 0 becomes 1 as soon as we click, click again 1 becomes 2 etc
-    i = i + 1
+    i = i + 1;
     if (i >= images.length) {
         i = 0;
     }
-})
-
+});
 
 const draw = () => {
     // console.log("getting to here");
@@ -93,18 +88,22 @@ const draw = () => {
         // console.log("correctly logging", event)
         if (images[i].complete) {
             // console.log(currentX, currentY)
-            context.drawImage(images[i], currentX - 200, currentY - 300, 400, 600)
+            context.drawImage(
+                images[i],
+                currentX - 200,
+                currentY - 300,
+                400,
+                600
+            );
         }
 
         // 0.01 makes it super slow
-        currentX = currentX + (aimX - currentX) * 0.1
-        currentY = currentY + (aimY - currentY) * 0.1
-
+        currentX = currentX + (aimX - currentX) * 0.1;
+        currentY = currentY + (aimY - currentY) * 0.1;
     }
 
     // run draw on every frame
-    requestAnimationFrame(draw)
-}
+    requestAnimationFrame(draw);
+};
 
-
-draw()
+draw();
