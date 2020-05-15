f65090f4ebf6f8a3a7e39b7fa9249309414a8b24
Joshua Fielding <Joshua.Fielding@fairfaxmedia.com.au>
2020-05-15 13:56:23 +1000

    added screen cover effect

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import style from "./sass/index.scss";
 import SmoothScrollingArticle, { Item } from "future-scroll";
 
 import "./js/accordions";
-// import "./js/cursor";
+import "./js/cursor";
 import "./js/collage";
 import "./js/elements-move";
 import "./js/gallery";
@@ -12,10 +12,10 @@ import "./js/nav";
 import "./js/mouse-move-shadow";
 
 // Add in wacky console message
-console.log(
-  "%c This is an experimentation site of Joshua Fielding, a frontend developer, reach out at josh.e.fielding@gmail.com",
-  "font-size: 50px;background:red;text-shadow:10px 10px 0 blue"
-);
+// console.log(
+//   "%c This is an experimentation site of Joshua Fielding, a frontend developer, reach out at josh.e.fielding@gmail.com",
+//   "font-size: 50px;background:red;text-shadow:10px 10px 0 blue"
+// );
 
 // Future Scroll
 const map = (x, a, b, c, d) => ((x - a) * (d - c)) / (b - a) + c;
@@ -24,9 +24,9 @@ const map = (x, a, b, c, d) => ((x - a) * (d - c)) / (b - a) + c;
 const parallaxScroll = document.querySelectorAll(".parallax-scroll");
 const items = [];
 
-parallaxScroll.forEach(element => {
+parallaxScroll.forEach((element) => {
   const item = new Item({
-    element
+    element,
   });
   const change = element.getAttribute("data-max-change")
     ? element.getAttribute("data-max-change")
@@ -40,7 +40,7 @@ parallaxScroll.forEach(element => {
       const val = map(currentPosition, 0, 0.9, change, change * -1);
       title.style.transform = `translate3d(0,${val}px,0)`;
       title.style.opacity = currentPosition;
-    }
+    },
   });
   items.push(item);
 });
@@ -70,5 +70,5 @@ new SmoothScrollingArticle({
   article: document.querySelector("article"),
   scrollable: document.querySelector(".scrolling-area"),
   items: items,
-  fixedParents: [document.querySelector("#heading-move")]
+  fixedParents: [document.querySelector("#heading-move")],
 });
--- src/js/cursor/index.js
+++ src/js/cursor/index.js
@@ -1,96 +1,67 @@
-// set the starting position of the cursor outside of the screen
-let clientX = -100;
-let clientY = -100;
-const innerCursor = document.querySelector(".cursor--small");
+var bridge = document.getElementById("screen-cover"),
+  bridgeCanvas = bridge.getContext("2d"),
+  brushRadius = (bridge.width / 100) * 2;
 
-const initCursor = () => {
-  // add listener to track the current mouse position
-  document.addEventListener("mousemove", e => {
-    clientX = e.clientX;
-    clientY = e.clientY;
-  });
-  
-  // transform the innerCursor to the current mouse position
-  // use requestAnimationFrame() for smooth performance
-  const render = () => {
-    innerCursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
-    // if you are already using TweenMax in your project, you might as well
-    // use TweenMax.set() instead
-    // TweenMax.set(innerCursor, {
-    //   x: clientX,
-    //   y: clientY
-    // });
-    
-    requestAnimationFrame(render);
-  };
-  requestAnimationFrame(render);
-};
+if (brushRadius < 15) {
+  brushRadius = 15;
+}
 
-initCursor();
+bridgeCanvas.fillStyle = "black";
+bridgeCanvas.fillRect(0, 0, bridge.width, bridge.height);
 
-
-
-// canvas part
-let lastX = 0;
-let lastY = 0;
-let isStuck = false;
-let showCursor = false;
-let group, stuckX, stuckY, fillOuterCursor;
-
-const initCanvas = () => {
-  const canvas = document.querySelector(".cursor--canvas");
-  const shapeBounds = {
-    width: 75,
-    height: 75
-  };
-  paper.setup(canvas);
-  const strokeColor = "rgba(255, 0, 0, 0.5)";
-  const strokeWidth = 1;
-  const segments = 8;
-  const radius = 15;
-  
-  // we'll need these later for the noisy circle
-  const noiseScale = 150; // speed
-  const noiseRange = 4; // range of distortion
-  let isNoisy = false; // state
-  
-  // the base shape for the noisy circle
-  const polygon = new paper.Path.RegularPolygon(
-    new paper.Point(0, 0),
-    segments,
-    radius
-  );
-  polygon.strokeColor = strokeColor;
-  polygon.strokeWidth = strokeWidth;
-  polygon.smooth();
-  group = new paper.Group([polygon]);
-  group.applyMatrix = false;
-  
-  const noiseObjects = polygon.segments.map(() => new SimplexNoise());
-  let bigCoordinates = [];
-  
-  // function for linear interpolation of values
-  const lerp = (a, b, n) => {
-    return (1 - n) * a + n * b;
-  };
-  
-  // function to map a value from one range to another range
-  const map = (value, in_min, in_max, out_min, out_max) => {
-    return (
-      ((value - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-    );
-  };
-  
-  // the draw loop of Paper.js 
-  // (60fps with requestAnimationFrame under the hood)
-  paper.view.onFrame = event => {
-    // using linear interpolation, the circle will move 0.2 (20%)
-    // of the distance between its current position and the mouse
-    // coordinates per Frame
-    lastX = lerp(lastX, clientX, 0.2);
-    lastY = lerp(lastY, clientY, 0.2);
-    group.position = new paper.Point(lastX, lastY);
+function detectLeftButton(event) {
+  if ("buttons" in event) {
+    return event.buttons === 1;
+  } else if ("which" in event) {
+    return event.which === 1;
+  } else {
+    return event.button === 1;
   }
 }
 
-initCanvas();
+function getBrushPos(xRef, yRef) {
+  var bridgeRect = bridge.getBoundingClientRect();
+  return {
+    x: Math.floor(
+      ((xRef - bridgeRect.left) / (bridgeRect.right - bridgeRect.left)) *
+        bridge.width
+    ),
+    y: Math.floor(
+      ((yRef - bridgeRect.top) / (bridgeRect.bottom - bridgeRect.top)) *
+        bridge.height
+    ),
+  };
+}
+
+function drawDot(mouseX, mouseY) {
+  bridgeCanvas.beginPath();
+  bridgeCanvas.arc(mouseX, mouseY, brushRadius, 0, 2 * Math.PI, true);
+  bridgeCanvas.fillStyle = "#000";
+  bridgeCanvas.globalCompositeOperation = "destination-out";
+  bridgeCanvas.fill();
+}
+
+bridge.addEventListener(
+  "mousemove",
+  function (e) {
+    var brushPos = getBrushPos(e.clientX, e.clientY);
+    var leftBut = detectLeftButton(e);
+    if (leftBut == 1) {
+      drawDot(brushPos.x, brushPos.y);
+    }
+  },
+  false
+);
+
+bridge.addEventListener(
+  "touchmove",
+  function (e) {
+    e.preventDefault();
+    var touch = e.targetTouches[0];
+    if (touch) {
+      var brushPos = getBrushPos(touch.pageX, touch.pageY);
+      drawDot(brushPos.x, brushPos.y);
+    }
+  },
+  false
+);
