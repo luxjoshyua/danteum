a8af8487ada31c73f90340b7c34d4d262eda3078
Joshua Fielding <Joshua.Fielding@fairfaxmedia.com.au>
2020-05-19 12:00:13 +1000

    added gallery two

--- src/js/gallery/index.js
+++ src/js/gallery/index.js
@@ -1,24 +1,39 @@
 new Swiper(".gallery .swiper-container", {
-    speed: 300,
-    spaceBetween: 160,
-    slidesPerView: 2,
-    initialSlide: 0,
-    draggable: true,
-    breakpoints: {
-        600: {
-            slidesPerView: 2
-        },
-        1000: {
-            slidesPerView: 3
-        }
-    }
-    // on: {
-    //     init: function () {
-    //         setSwiperClasses(this);
-    //     },
-    //     slideChange: function () {
-    //         setSwiperClasses(this);
-    //     }
-    // }
+  speed: 300,
+  spaceBetween: 160,
+  slidesPerView: 2,
+  initialSlide: 0,
+  draggable: true,
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
+  },
 });
 
+new Swiper(".gallery-two .swiper-container", {
+  speed: 300,
+  spaceBetween: 20,
+  slidesPerView: 1,
+  initialSlide: 0,
+  draggable: true,
+  breakpoints: {
+    600: {
+      slidesPerView: 1,
+    },
+    1000: {
+      slidesPerView: 1,
+    },
+  },
+  // on: {
+  //     init: function () {
+  //         setSwiperClasses(this);
+  //     },
+  //     slideChange: function () {
+  //         setSwiperClasses(this);
+  //     }
+  // }
+});
