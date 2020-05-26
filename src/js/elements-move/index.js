0e6f44b5f8a79fdb8699341ae6c61495f5f55539
Aaron Lutze <aaron.lutze.8@gmail.com>
2020-05-26 12:16:20 +1000

    Set text scrolling only if in view

--- src/js/elements-move/index.js
+++ src/js/elements-move/index.js
@@ -5,17 +5,19 @@ const heroSection = document.querySelector(".hero-first-section");
 const heroSectionHeight = heroSection.getBoundingClientRect().height;
 
 const widthText = heading.getBoundingClientRect().width;
+const headingWidth = heading.clientWidth;
 
 // if we move 1px, will go left widthText / heroSectionHeight;
 const calculateMovement = widthText / heroSectionHeight;
 // console.log(calculateMovement);
 
-const headingMove = () => {
-  const scrollTop = heroSection.getBoundingClientRect().top;
-  // console.log(scrollTop);
+function headingMove(){
+  const scrollTop = Math.round( window.scrollY );
   // for every 1px scrolled, we need to use var calculateMovement
-  const mathMovement = calculateMovement * scrollTop;
-  heading.style.transform = `translateX(${mathMovement}px)`;
+  const mathMovement = Math.round( calculateMovement * scrollTop * -1 );
+  if( mathMovement >= ( headingWidth  * -1) ){
+    heading.style.transform = `translateX(${mathMovement}px)`;
+  }
 };
 
 // update position of heading when scroll event fires
